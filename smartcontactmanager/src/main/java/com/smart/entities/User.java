18ce530ca74a507e72b9db39732ad2d6d4aba333
Vivek Kumar <vivekchaudhary887735@gmail.com>
2023-10-23 18:17:21 +0530

    Done

--- smartcontactmanager/src/main/java/com/smart/entities/User.java
+++ smartcontactmanager/src/main/java/com/smart/entities/User.java
@@ -14,7 +14,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @Entity
