437d2dbfc6fa3bfa3b500e8c43978adbf3a262ca
Vivek Kumar <vivekchaudhary887735@gmail.com>
2023-10-23 17:26:16 +0530

    done

--- smartcontactmanager/src/main/java/com/smart/config/MyConfig.java
+++ smartcontactmanager/src/main/java/com/smart/config/MyConfig.java
@@ -3,16 +3,17 @@ package com.smart.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class MyConfig extends WebSecurityConfigurerAdapter {
+public class MyConfig {
     
     @Bean
     public UserDetailsService getUserDetailsService() {
@@ -32,17 +33,23 @@ public class MyConfig extends WebSecurityConfigurerAdapter {
         return daoAuthenticationProvider;
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/user/**").hasRole("USER")
+                .requestMatchers("/**").permitAll().and().formLogin(withDefaults()).csrf(csrf -> csrf.disable());
+        // http.csrf().disable().authorizeHttpRequests().requestMatchers("/admin/**").hasRole("ADMIN")
+        //     .requestMatchers("/user/**").hasRole("USER")
+        //     .requestMatchers("/**").permitAll().and().formLogin();
+        return http.build();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
-        .antMatchers("/user/**").hasRole("USER")
-        .antMatchers("/**").permitAll().and().formLogin().and().csrf().disable();
-    }
+    // @Override
+    // protected void configure(HttpSecurity http) throws Exception {
+    //     http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
+    //     .antMatchers("/user/**").hasRole("USER")
+    //     .antMatchers("/**").permitAll().and().formLogin().and().csrf().disable();
+    // }
 
     
 }
